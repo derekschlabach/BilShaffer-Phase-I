06bba7f73bf6c84efc48c26f5da452795fc7bde7
Derek J. Schlabach <derek.schlabach@cslab14.wheaton.edu>
2015-01-29 20:07:48 -0600

    Made some semblence of a skeleton

---
+++ RunTests.py
@@ -0,0 +1,12 @@
+from BCTests import *
+from MeshTests import *
+from MeshFactoryTests import *
+import unittest
+
+
+testSuite = unittest.makeSuite(TestBC)
+testSuite.addTest(unittest.makesuite(TestMesh))
+testSuite.addTest(unittest.makesuite(TestMeshFactory))
+
+testRunner = unittest.TextTestRunner()
+testRunner.run(testSuite)
