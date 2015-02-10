ee77ae3d1c5da850bfef20cd84c4cb6be28af38a
derekschlabach <derek.schlabach@my.wheaton.edu>
2015-02-09 21:26:09 -0600

    Added a bunch of empty files.

--- MeshTests.py
+++ MeshTests.py
@@ -4,18 +4,18 @@ import MeshFactory
 import Mesh
 import BF
 import VarFactory
+import PoissonFormulation
 
 class TestMesh(unittest.TestCase):
     
     def testCellPolyOrder(self):
 
-        #Mesh.vectori(2
-        dimensions = MeshFactory.vectord(2, 2.3)
-        elements = MeshFactory.vectori(2,2)
-        bf = BF.BF_bf(VarFactory.VarFactory()) #ToDo Give the VarFactory a field & test variable
-        testMesh = MeshFactory.MeshFactory_rectilinearMesh(bf, dimensions, elements, 1) 
 
-        print testMesh.cellPolyOrder()
+        poissonForm = PoissonFormulation.PoissonFormulation(2, True)
+        poissonBF = poissonForm.bf() #ToDo Give the VarFactory a field & test variable
+        testMesh = MeshFactory.MeshFactory_rectilinearMesh(poissonBF, [1.3, 1.4], [2,3], 1) 
+
+        print testMesh.cellPolyOrder(0)
         
 
 if (__name__ == '__main__'):
